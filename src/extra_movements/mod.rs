cf0140ccd090305ef8dc83ac5b89eb169d6e8c1d
Yaskhan <yaskhan@yandex.ru>
2026-01-29 19:23:10 +0500

    Integrate Jetpack System
    Registered JetpackPlugin in src/extra_movements/mod.rs.

--- src/extra_movements/mod.rs
+++ src/extra_movements/mod.rs
@@ -1,4 +1,5 @@
 pub mod fly;
+pub mod jetpack;
 
 use bevy::prelude::*;
 
@@ -6,6 +7,7 @@ pub struct ExtraMovementsPlugin;
 
 impl Plugin for ExtraMovementsPlugin {
     fn build(&self, app: &mut App) {
-        app.add_plugins(fly::FlyPlugin);
+        app.add_plugins(fly::FlyPlugin)
+           .add_plugins(jetpack::JetpackPlugin);
     }
 }
