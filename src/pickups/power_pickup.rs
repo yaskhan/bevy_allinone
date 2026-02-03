95929192df3da0ed4d49e2363ec0220d6f21c2bf
Yaskhan <yaskhan@yandex.ru>
2026-02-03 09:07:03 +0500

    Expand power pickup data

--- src/pickups/power_pickup.rs
+++ src/pickups/power_pickup.rs
@@ -2,17 +2,19 @@ use bevy::prelude::*;
 
 /// Power pickup data.
 ///
-/// GKC reference: `powerPickup.cs`
+///
 #[derive(Component, Debug, Reflect)]
 #[reflect(Component)]
 pub struct PowerPickup {
     pub power_id: String,
+    pub power_name: String,
 }
 
 impl Default for PowerPickup {
     fn default() -> Self {
         Self {
             power_id: String::new(),
+            power_name: String::new(),
         }
     }
 }
