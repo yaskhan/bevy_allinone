fc089156c6690fd22e27e3a670e2bafa941d54da
Yaskhan <yaskhan@yandex.ru>
2026-02-03 09:06:37 +0500

    Expand map pickup data

--- src/pickups/map_pickup.rs
+++ src/pickups/map_pickup.rs
@@ -2,17 +2,19 @@ use bevy::prelude::*;
 
 /// Map pickup data.
 ///
-/// GKC reference: `mapPickup.cs`
+///
 #[derive(Component, Debug, Reflect)]
 #[reflect(Component)]
 pub struct MapPickup {
     pub map_id: String,
+    pub map_name: String,
 }
 
 impl Default for MapPickup {
     fn default() -> Self {
         Self {
             map_id: String::new(),
+            map_name: String::new(),
         }
     }
 }
