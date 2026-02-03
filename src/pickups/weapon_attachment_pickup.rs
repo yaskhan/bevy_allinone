47d8bda5a1a3bd2314e6b67aa200d9c4918bab21
Yaskhan <yaskhan@yandex.ru>
2026-02-03 09:04:52 +0500

    Expand weapon attachment pickup data

--- src/pickups/weapon_attachment_pickup.rs
+++ src/pickups/weapon_attachment_pickup.rs
@@ -2,17 +2,19 @@ use bevy::prelude::*;
 
 /// Weapon attachment pickup data.
 ///
-/// GKC reference: `weaponAttachmentPickup.cs`
+///
 #[derive(Component, Debug, Reflect)]
 #[reflect(Component)]
 pub struct WeaponAttachmentPickup {
     pub attachment_id: String,
+    pub attachment_name: String,
 }
 
 impl Default for WeaponAttachmentPickup {
     fn default() -> Self {
         Self {
             attachment_id: String::new(),
+            attachment_name: String::new(),
         }
     }
 }
