644a35cd61133b6703385d93cc36ca68bbe6b36f
Yaskhan <yaskhan@yandex.ru>
2026-02-03 09:04:28 +0500

    Expand melee shield pickup data

--- src/pickups/melee_shield_pickup.rs
+++ src/pickups/melee_shield_pickup.rs
@@ -7,12 +7,14 @@ use bevy::prelude::*;
 #[reflect(Component)]
 pub struct MeleeShieldPickup {
     pub shield_id: String,
+    pub shield_name: String,
 }
 
 impl Default for MeleeShieldPickup {
     fn default() -> Self {
         Self {
             shield_id: String::new(),
+            shield_name: String::new(),
         }
     }
 }
