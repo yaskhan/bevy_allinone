103bd5fbddc10c2ce7388f5f5c576382dfe5ba18
Yaskhan <yaskhan@yandex.ru>
2026-02-03 08:57:11 +0500

    Expand pickup icon info

--- src/pickups/pickup_icon_info.rs
+++ src/pickups/pickup_icon_info.rs
@@ -6,15 +6,27 @@ use bevy::prelude::*;
 #[derive(Component, Debug, Reflect)]
 #[reflect(Component)]
 pub struct PickUpIconInfo {
-    pub icon_path: String,
-    pub size: Vec2,
+    pub id: i32,
+    pub name: String,
+    pub icon_object: Option<Entity>,
+    pub texture_object: Option<Entity>,
+    pub target: Option<Entity>,
+    pub icon_active: bool,
+    pub paused: bool,
+    pub icon_size: Vec2,
 }
 
 impl Default for PickUpIconInfo {
     fn default() -> Self {
         Self {
-            icon_path: String::new(),
-            size: Vec2::new(32.0, 32.0),
+            id: 0,
+            name: String::new(),
+            icon_object: None,
+            texture_object: None,
+            target: None,
+            icon_active: false,
+            paused: false,
+            icon_size: Vec2::new(32.0, 32.0),
         }
     }
 }
