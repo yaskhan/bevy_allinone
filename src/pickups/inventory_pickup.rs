019f06f2e90019776a91f33a42e429d03a944980
Yaskhan <yaskhan@yandex.ru>
2026-02-03 09:05:18 +0500

    Expand inventory pickup data

--- src/pickups/inventory_pickup.rs
+++ src/pickups/inventory_pickup.rs
@@ -2,11 +2,12 @@ use bevy::prelude::*;
 
 /// Inventory pickup data.
 ///
-/// GKC reference: `inventoryPickup.cs`
+///
 #[derive(Component, Debug, Reflect)]
 #[reflect(Component)]
 pub struct InventoryPickup {
     pub item_id: String,
+    pub item_name: String,
     pub quantity: i32,
 }
 
@@ -14,6 +15,7 @@ impl Default for InventoryPickup {
     fn default() -> Self {
         Self {
             item_id: String::new(),
+            item_name: String::new(),
             quantity: 1,
         }
     }
