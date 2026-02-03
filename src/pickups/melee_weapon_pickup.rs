bb160123836c7f729b4746f0ed5f33db17c4f8eb
Yaskhan <yaskhan@yandex.ru>
2026-02-03 09:04:03 +0500

    Expand melee weapon pickup data

--- src/pickups/melee_weapon_pickup.rs
+++ src/pickups/melee_weapon_pickup.rs
@@ -7,12 +7,14 @@ use bevy::prelude::*;
 #[reflect(Component)]
 pub struct MeleeWeaponPickup {
     pub weapon_id: String,
+    pub weapon_name: String,
 }
 
 impl Default for MeleeWeaponPickup {
     fn default() -> Self {
         Self {
             weapon_id: String::new(),
+            weapon_name: String::new(),
         }
     }
 }
