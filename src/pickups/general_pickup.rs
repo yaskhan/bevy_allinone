938193dfd56c2818ba81ad9bc734aa87c0ac5830
Yaskhan <yaskhan@yandex.ru>
2026-02-03 08:54:38 +0500

    Expand general pickup data

--- src/pickups/general_pickup.rs
+++ src/pickups/general_pickup.rs
@@ -6,13 +6,34 @@ use bevy::prelude::*;
 #[derive(Component, Debug, Reflect)]
 #[reflect(Component)]
 pub struct GeneralPickup {
-    pub pickup_id: String,
+    pub stat_info_list: Vec<PickupStatInfo>,
+    pub pickup_name: String,
 }
 
 impl Default for GeneralPickup {
     fn default() -> Self {
         Self {
-            pickup_id: String::new(),
+            stat_info_list: Vec::new(),
+            pickup_name: String::new(),
+        }
+    }
+}
+
+#[derive(Debug, Clone, Reflect)]
+pub struct PickupStatInfo {
+    pub name: String,
+    pub amount_to_add: f32,
+    pub use_remote_event: bool,
+    pub remote_event_list: Vec<String>,
+}
+
+impl Default for PickupStatInfo {
+    fn default() -> Self {
+        Self {
+            name: String::new(),
+            amount_to_add: 0.0,
+            use_remote_event: false,
+            remote_event_list: Vec::new(),
         }
     }
 }
