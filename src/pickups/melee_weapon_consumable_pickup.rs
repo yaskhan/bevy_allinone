6f13b60de8ae110d8b1d18d16da5784be8c402c1
Yaskhan <yaskhan@yandex.ru>
2026-02-03 09:23:38 +0500

    Expand melee weapon consumable pickup

--- src/pickups/melee_weapon_consumable_pickup.rs
+++ src/pickups/melee_weapon_consumable_pickup.rs
@@ -7,10 +7,16 @@ use bevy::prelude::*;
 #[reflect(Component)]
 pub struct MeleeWeaponConsumablePickup {
     pub amount: i32,
+    pub weapon_consumable_name: String,
+    pub store_picked_weapons_on_inventory: bool,
 }
 
 impl Default for MeleeWeaponConsumablePickup {
     fn default() -> Self {
-        Self { amount: 0 }
+        Self {
+            amount: 0,
+            weapon_consumable_name: String::new(),
+            store_picked_weapons_on_inventory: false,
+        }
     }
 }
