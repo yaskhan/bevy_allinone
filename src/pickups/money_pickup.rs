cbce16916efd535a3c0b8223deb0ecf1269426fb
Yaskhan <yaskhan@yandex.ru>
2026-02-03 09:05:44 +0500

    Expand money pickup data

--- src/pickups/money_pickup.rs
+++ src/pickups/money_pickup.rs
@@ -2,15 +2,19 @@ use bevy::prelude::*;
 
 /// Money pickup data.
 ///
-/// GKC reference: `moneyPickup.cs`
+///
 #[derive(Component, Debug, Reflect)]
 #[reflect(Component)]
 pub struct MoneyPickup {
     pub amount: f32,
+    pub currency_type: String,
 }
 
 impl Default for MoneyPickup {
     fn default() -> Self {
-        Self { amount: 0.0 }
+        Self {
+            amount: 0.0,
+            currency_type: String::new(),
+        }
     }
 }
