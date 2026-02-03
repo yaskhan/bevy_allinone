873a7c25322a24cc7cf83c255239e19df2e80b5a
Yaskhan <yaskhan@yandex.ru>
2026-02-03 12:44:16 +0500

    Expand melee weapon inventory data

--- src/inventory/melee_weapon_on_inventory.rs
+++ src/inventory/melee_weapon_on_inventory.rs
@@ -7,12 +7,18 @@ use bevy::prelude::*;
 #[reflect(Component)]
 pub struct MeleeWeaponOnInventory {
     pub weapon_id: String,
+    pub weapon_name: String,
+    pub prefab_path: String,
+    pub icon_path: String,
 }
 
 impl Default for MeleeWeaponOnInventory {
     fn default() -> Self {
         Self {
             weapon_id: String::new(),
+            weapon_name: String::new(),
+            prefab_path: String::new(),
+            icon_path: String::new(),
         }
     }
 }
