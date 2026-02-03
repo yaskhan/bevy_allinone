a3204f4e3ed474e7ca19c9d57eeeeb04dd93ab97
Yaskhan <yaskhan@yandex.ru>
2026-02-03 08:57:38 +0500

    Expand player pickup icon manager

--- src/pickups/player_pickup_icon_manager.rs
+++ src/pickups/player_pickup_icon_manager.rs
@@ -1,4 +1,5 @@
 use bevy::prelude::*;
+use super::PickUpIconInfo;
 
 /// Manages pickup icons for player.
 ///
@@ -6,11 +7,19 @@ use bevy::prelude::*;
 #[derive(Component, Debug, Reflect)]
 #[reflect(Component)]
 pub struct PlayerPickupIconManager {
-    pub visible: bool,
+    pub show_icons_active: bool,
+    pub show_icons_paused: bool,
+    pub max_distance_icon_enabled: f32,
+    pub pick_up_icon_list: Vec<PickUpIconInfo>,
 }
 
 impl Default for PlayerPickupIconManager {
     fn default() -> Self {
-        Self { visible: true }
+        Self {
+            show_icons_active: true,
+            show_icons_paused: false,
+            max_distance_icon_enabled: 10.0,
+            pick_up_icon_list: Vec::new(),
+        }
     }
 }
