c52c326ec34714d69d67190cc687b19667015ab6
Yaskhan <yaskhan@yandex.ru>
2026-02-03 09:06:11 +0500

    Expand experience pickup data

--- src/pickups/experience_pickup.rs
+++ src/pickups/experience_pickup.rs
@@ -2,15 +2,19 @@ use bevy::prelude::*;
 
 /// Experience pickup data.
 ///
-/// GKC reference: `experiencePickup.cs`
+///
 #[derive(Component, Debug, Reflect)]
 #[reflect(Component)]
 pub struct ExperiencePickup {
     pub amount: u32,
+    pub use_multiplier: bool,
 }
 
 impl Default for ExperiencePickup {
     fn default() -> Self {
-        Self { amount: 0 }
+        Self {
+            amount: 0,
+            use_multiplier: true,
+        }
     }
 }
