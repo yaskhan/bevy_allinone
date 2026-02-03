dba574a748a3968799a6eea02797d94d1f473da4
Yaskhan <yaskhan@yandex.ru>
2026-02-03 08:55:52 +0500

    Expand pickup manager data

--- src/pickups/pickup_manager.rs
+++ src/pickups/pickup_manager.rs
@@ -1,4 +1,5 @@
 use bevy::prelude::*;
+use super::{PickUpElementInfo, PickUpIconInfo};
 
 /// Manages pickup list and settings.
 ///
@@ -6,15 +7,21 @@ use bevy::prelude::*;
 #[derive(Component, Debug, Reflect)]
 #[reflect(Component)]
 pub struct PickUpManager {
-    pub enabled: bool,
-    pub pickups: Vec<Entity>,
+    pub show_icons_active: bool,
+    pub main_pickup_list: Vec<PickUpElementInfo>,
+    pub pickup_icon_list: Vec<PickUpIconInfo>,
+    pub player_pickup_icon_manager_list: Vec<Entity>,
+    pub current_id: i32,
 }
 
 impl Default for PickUpManager {
     fn default() -> Self {
         Self {
-            enabled: true,
-            pickups: Vec::new(),
+            show_icons_active: true,
+            main_pickup_list: Vec::new(),
+            pickup_icon_list: Vec::new(),
+            player_pickup_icon_manager_list: Vec::new(),
+            current_id: 0,
         }
     }
 }
